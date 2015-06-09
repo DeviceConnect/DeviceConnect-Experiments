7fd330b2af073f28393b0aa8b40dd617eacfcb46
小林伸郎 <nobu@gclue.jp>
2015-06-09 10:24:53 +0900

    Updated device connect sdk

--- MWC2015/demoWebSite/js/profile/servicediscovery.js
+++ MWC2015/demoWebSite/js/profile/servicediscovery.js
@@ -4,33 +4,42 @@
  Released under the MIT license
  http://opensource.org/licenses/mit-license.php
  */
- 
 
 /**
  * デバイスの検索
  */
 function searchDevice() {
-    initAll();
+  initAll();
+  closeLoading();
+  showLoading();
+
+  dConnect.setHost(ip);
+  dConnect.discoverDevices(accessToken, function(obj) {
+    var str = '';
+    if (DEBUG) {
+      console.log('response: ', obj);
+    }
+
+    for (var i = 0; i < obj.services.length; i++) {
+      str += '<li><a href="javascript:searchSystem(\'' +
+              obj.services[i].id + '\',\'' + obj.services[i].name +
+              '\');" value="' + obj.services[i].name + '">' +
+              obj.services[i].name + '</a></li>';
+    }
+
+    setTitle('Device List', 'black');
+    deleteMode = false;
+
+    var listHtml = document.getElementById('list');
+    listHtml.innerHTML = str;
+    $('ul.list').listview('refresh');
     closeLoading();
-    showLoading();
-
-    dConnect.setHost(ip);
-    dConnect.discoverDevices(accessToken, function(obj){
-        var str = "";
-        if(DEBUG) console.log("response: ", obj);
-
-        for (var i = 0; i < obj.services.length; i++) {
-            str += '<li><a href="javascript:searchSystem(\'' + obj.services[i].id + '\',\'' + obj.services[i].name + '\');" value="' + obj.services[i].name + '">' + obj.services[i].name + '</a></li>';
-        }
-
-        setTitle("Device List", "black");
-        deleteMode = false;
-
-        var listHtml = document.getElementById('list');
-        listHtml.innerHTML = str;
-        $("ul.list").listview("refresh");
-        closeLoading();
-    }, function(errorCode, errorMessage) {
-        alert("Error: code=" + errorCode + ", messsage=\"" + errorMessage + "\"");
-    });
+  }, function(errorCode, errorMessage) {
+    closeLoading();
+    if (errorCode == 12 || errorCode == 13 || errorCode == 15) {
+      authorization(searchDevice);
+    } else {
+      alert('Error: code=' + errorCode + ', messsage=\"' + errorMessage + '\"');
+    }
+  });
 }
