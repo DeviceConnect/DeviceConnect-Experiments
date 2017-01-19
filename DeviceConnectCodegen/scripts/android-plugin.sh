4da74110a4fa358bb8d0102073de74d51a57d698
Masaru Takano <mtaka@gclue.jp>
2017-01-19 15:25:51 +0900

    mvnコマンドで配布用zipを生成できるように修正。

--- DeviceConnectCodegen/scripts/android-plugin.sh
+++ DeviceConnectCodegen/scripts/android-plugin.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -x
 
-SPEC_DIR="spec"
-OUTPUT_DIR="output/Android/MyPlugin"
+LANG="deviceConnectAndroidPlugin"
+SPEC_DIR="./spec"
+OUTPUT_DIR="./output/Android/MyPlugin"
 PACKAGE_NAME="com.mydomain.myplugin"
 DISPLAY_NAME="MyPlugin"
-JAR_FILE="bin/deviceconnect-codegen-cli.jar"
-ARGS="--input-spec-dir $SPEC_DIR  --lang deviceConnectAndroidPlugin --package-name  $PACKAGE_NAME --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
+
+JAR_FILE="./bin/deviceconnect-codegen.jar"
+ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --package-name  $PACKAGE_NAME --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
 
 java -Dfile.encoding=UTF-8 -jar $JAR_FILE $ARGS
--- DeviceConnectCodegen/scripts/ios-plugin.sh
+++ DeviceConnectCodegen/scripts/ios-plugin.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -x
 
-executable="bin/deviceconnect-codegen-cli.jar"
-args="--input-spec-dir spec  --lang deviceConnectIosPlugin  --display-name MyPlugin  --output output/iOS/MyPlugin"
+LANG="deviceConnectIosPlugin"
+SPEC_DIR="./spec"
+OUTPUT_DIR="./output/iOS/MyPlugin"
+DISPLAY_NAME="MyPlugin"
 
-java -Dfile.encoding=UTF-8 -jar $executable $args
+JAR_FILE="./bin/deviceconnect-codegen.jar"
+ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --package-name  $PACKAGE_NAME --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
+
+java -Dfile.encoding=UTF-8 -jar $JAR_FILE $ARGS
