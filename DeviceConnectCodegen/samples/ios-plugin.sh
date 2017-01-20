ac6540c795ce6372e871342e6e58092fca96980e
Masaru Takano <mtaka@gclue.jp>
2017-01-20 17:41:44 +0900

    バグ修正: iOSプラグイン生成時に --package-nameオプションは不要。

--- DeviceConnectCodegen/samples/ios-plugin.sh
+++ DeviceConnectCodegen/samples/ios-plugin.sh
@@ -6,6 +6,6 @@ OUTPUT_DIR="./output/iOS/MyPlugin"
 DISPLAY_NAME="MyPlugin"
 
 JAR_FILE="../bin/deviceconnect-codegen.jar"
-ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --package-name  $PACKAGE_NAME --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
+ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
 
 java -Dfile.encoding=UTF-8 -jar $JAR_FILE $ARGS
