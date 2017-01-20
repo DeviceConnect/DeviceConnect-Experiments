aca472668910ce109ca648c494ab1a017eaaa33c
Masaru Takano <mtaka@gclue.jp>
2017-01-20 12:55:02 +0900

    サンプルのプロファイル定義ファイルを追加。

--- DeviceConnectCodegen/samples/android-plugin.sh
+++ DeviceConnectCodegen/samples/android-plugin.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -x
 
 LANG="deviceConnectAndroidPlugin"
-SPEC_DIR="./spec"
+SPEC_DIR="./sample-profile-specs"
 OUTPUT_DIR="./output/Android/MyPlugin"
 PACKAGE_NAME="com.mydomain.myplugin"
 DISPLAY_NAME="MyPlugin"
 
-JAR_FILE="./bin/deviceconnect-codegen.jar"
+JAR_FILE="../bin/deviceconnect-codegen.jar"
 ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --package-name  $PACKAGE_NAME --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
 
 java -Dfile.encoding=UTF-8 -jar $JAR_FILE $ARGS
