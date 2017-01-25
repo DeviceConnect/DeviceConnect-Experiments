d431af959dfec8e4e92cb112a0c714aaaf6e6e5c
Masaru Takano <mtaka@gclue.jp>
2017-01-25 19:06:22 +0900

    DeviceConnectCodegenのサンプルシェルスクリプトにコメントを追記。

--- DeviceConnectCodegen/samples/android-plugin.sh
+++ DeviceConnectCodegen/samples/android-plugin.sh
@@ -1,12 +1,23 @@
 #!/bin/sh -x
 
+# スケルトンコード種別: Androidプラグイン
 LANG="deviceConnectAndroidPlugin"
+
+# プロファイル定義ファイルのディレクトリ
 SPEC_DIR="./sample-profile-specs"
+
+# スケルトンコード出力先
 OUTPUT_DIR="./output/Android/MyPlugin"
+
+# Androidプラグインのパッケージ名
 PACKAGE_NAME="com.mydomain.myplugin"
+
+# Androidプラグインの表示名
 DISPLAY_NAME="MyPlugin"
 
+# スケルトンコード生成ツールのバイナリ
 JAR_FILE="../bin/deviceconnect-codegen.jar"
+
 ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --package-name  $PACKAGE_NAME --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
 
 java -Dfile.encoding=UTF-8 -jar $JAR_FILE $ARGS
--- DeviceConnectCodegen/samples/ios-plugin.sh
+++ DeviceConnectCodegen/samples/ios-plugin.sh
@@ -1,11 +1,20 @@
 #!/bin/sh -x
 
+# スケルトンコード種別: iOSプラグイン
 LANG="deviceConnectIosPlugin"
+
+# プロファイル定義ファイルのディレクトリ
 SPEC_DIR="./sample-profile-specs"
+
+# スケルトンコード出力先
 OUTPUT_DIR="./output/iOS/MyPlugin"
+
+# iOSプラグインの表示名
 DISPLAY_NAME="MyPlugin"
 
+# スケルトンコード生成ツールのバイナリ
 JAR_FILE="../bin/deviceconnect-codegen.jar"
-ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --display-name $DISPLAY_NAME  --output $OUTPUT_DIR"
+
+ARGS="--input-spec-dir $SPEC_DIR  --lang $LANG --display-name $DISPLAY_NAME --output $OUTPUT_DIR"
 
 java -Dfile.encoding=UTF-8 -jar $JAR_FILE $ARGS
