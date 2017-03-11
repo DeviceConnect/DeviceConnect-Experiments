8d5e20df4f845126c96144aa927306df92ab9a5e
Masaru Takano <mtaka@gclue.jp>
2017-03-11 16:04:05 +0900

    DeviceConnectCodegenのバージョン変更: 0.7.0 -> 1.0.0
    配布用zipにソースコードを含める。

--- DeviceConnectCodegen/download-spec.sh
+++ DeviceConnectCodegen/download-spec.sh
@@ -7,9 +7,17 @@ SPEC_ZIP_NAME=${REVISION}.zip
 SPEC_ZIP_URL=https://github.com/TakayukiHoshi1984/DeviceConnect-Spec/archive/${SPEC_ZIP_NAME}
 SPEC_DIR=${PROJECT_DIR}/standard-profile-specs
 
-mkdir ${TEMP_DIR}
+if [ -e ${SPEC_DIR} ] ; then
+    echo "Delete ${SPEC_DIR}"
+    rm -rf ${SPEC_DIR}
+else
+    echo "Create ${SPEC_DIR}"
+    mkdir -p ${SPEC_DIR}
+fi
+
+mkdir -p ${TEMP_DIR}
 cd ${TEMP_DIR}
-wget ${SPEC_ZIP_URL}
+curl -L -O -k ${SPEC_ZIP_URL}
 unzip -d . ${SPEC_ZIP_NAME}
 cp ${TEMP_DIR}/DeviceConnect-Spec-${REVISION}/api/*.json ${SPEC_DIR}
 rm -rf ${TEMP_DIR}
