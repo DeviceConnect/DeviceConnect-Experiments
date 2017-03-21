05df3a57f076d989847a5e287cb2d9c0bae31bb3
Masaru Takano <mtaka@gclue.jp>
2017-03-21 21:07:57 +0900

    JSONのダウンロード先を更新、

--- DeviceConnectCodegen/download-spec.sh
+++ DeviceConnectCodegen/download-spec.sh
@@ -2,7 +2,8 @@
 
 PROJECT_DIR=$(cd $(dirname $0) && pwd)
 TEMP_DIR=${PROJECT_DIR}/temp-`date +%s`
-REVISION=master
+REVISION="v2.2.1-release-20170324"
+REVISION_UNZIPPED="2.2.1-release-20170324"
 SPEC_ZIP_NAME=${REVISION}.zip
 SPEC_ZIP_URL=https://github.com/TakayukiHoshi1984/DeviceConnect-Spec/archive/${SPEC_ZIP_NAME}
 SPEC_DIR=${PROJECT_DIR}/standard-profile-specs
@@ -19,5 +20,5 @@ mkdir -p ${TEMP_DIR}
 cd ${TEMP_DIR}
 curl -L -O -k ${SPEC_ZIP_URL}
 unzip -d . ${SPEC_ZIP_NAME}
-cp ${TEMP_DIR}/DeviceConnect-Spec-${REVISION}/api/*.json ${SPEC_DIR}
+cp ${TEMP_DIR}/DeviceConnect-Spec-${REVISION_UNZIPPED}/api/*.json ${SPEC_DIR}
 rm -rf ${TEMP_DIR}
