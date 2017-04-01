4d7e355357de217b9805d33b4a64906b10b75ef9
Masaru Takano <mtaka@gclue.jp>
2017-04-02 01:00:07 +0900

    HTMLドキュメント生成機能にレスポンス・イベントを追加。

--- DeviceConnectCodegen/modules/deviceconnect-codegen/src/main/java/org/deviceconnect/codegen/docs/HtmlDocsCodegenConfig.java
+++ DeviceConnectCodegen/modules/deviceconnect-codegen/src/main/java/org/deviceconnect/codegen/docs/HtmlDocsCodegenConfig.java
@@ -1,17 +1,22 @@
 package org.deviceconnect.codegen.docs;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.codegen.CodegenType;
 import io.swagger.codegen.DefaultCodegen;
 import io.swagger.codegen.SupportingFile;
-import io.swagger.models.HttpMethod;
-import io.swagger.models.Operation;
-import io.swagger.models.Path;
-import io.swagger.models.Swagger;
+import io.swagger.models.*;
 import io.swagger.models.parameters.FormParameter;
 import io.swagger.models.parameters.Parameter;
 import io.swagger.models.parameters.QueryParameter;
+import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.properties.ObjectProperty;
+import io.swagger.models.properties.Property;
+import io.swagger.models.properties.RefProperty;
 import org.deviceconnect.codegen.DConnectCodegenConfig;
+import org.deviceconnect.codegen.models.DConnectOperation;
 
 import java.util.*;
 
@@ -120,15 +125,15 @@ public class HtmlDocsCodegenConfig extends DefaultCodegen implements DConnectCod
                             String id = method + "-" + fullPathName.replaceAll("/", "-");
                             return id.toLowerCase();
                         }
-                        String name() {
-                            return method + " " + fullPathName;
-                        }
+                        String name = method + " " + fullPathName;
                         String type = (String) op.getVendorExtensions().get("x-type");
                         String summary = op.getSummary();
                         String description = op.getDescription();
                         List<Object> paramList() {
                             return paramList;
                         }
+                        Object response = createResponseDocument(profileSpec, op);
+                        Object event = createEventDocument(profileSpec, op);
                     });
                 }
             }
@@ -159,7 +164,208 @@ public class HtmlDocsCodegenConfig extends DefaultCodegen implements DConnectCod
         supportingFiles.add(new SupportingFile("html/all-operations.html.mustache", "html", "all-operations.html"));
     }
 
+    private Object createResponseDocument(final Swagger swagger, final Operation operation) {
+        final Map<String, Response> responses = operation.getResponses();
+        if (responses != null) {
+            Response response = responses.get("200");
+            if (response != null) {
+                Property schema = response.getSchema();
+                return createMessageDocument(swagger, schema);
+            }
+        }
+        return null;
+    }
+
+    private Object createEventDocument(final Swagger swagger, final Operation operation) {
+        DConnectOperation dConnectOperation = DConnectOperation.parse(swagger, operation);
+        if (dConnectOperation == null) {
+            return null;
+        }
+        Response eventModel = dConnectOperation.getEventModel();
+        if (eventModel == null) {
+            return null;
+        }
+        Property schema = eventModel.getSchema();
+        if (schema == null) {
+            return null;
+        }
+        return createMessageDocument(swagger, schema);
+    }
+
+    private Object createMessageDocument(final Swagger swagger, final Property schema) {
+        ObjectProperty root;
+        if (schema instanceof ObjectProperty) {
+            root = (ObjectProperty) schema;
+        } else if (schema instanceof RefProperty) {
+            RefProperty ref = (RefProperty) schema;
+            Model model = findDefinition(swagger, ref.getSimpleRef());
+            Map<String, Property> properties;
+            if (model instanceof ComposedModel) {
+                properties = getProperties(swagger, (ComposedModel) model);
+            } else if (model instanceof ModelImpl) {
+                properties = model.getProperties();
+            } else {
+                return null;
+            }
+            if (properties == null) {
+                return null;
+            }
+            root =  new ObjectProperty();
+            root.setProperties(properties);
+        } else {
+            return null;
+        }
+
+        final List<ResponseParamDoc> paramDocList = new ArrayList<>();
+        Map<String, Property> props = root.getProperties();
+        if (props != null && props.size() > 0) {
+            createResponseParameterDocument(root, paramDocList, 1);
+        }
+
+        final int maxNestLevel = getMaxNestLevel(paramDocList);
+        for (ResponseParamDoc paramDoc : paramDocList) {
+            paramDoc.setMaxNestLevel(maxNestLevel);
+        }
+
+        return new Object() {
+            List<ResponseParamDoc> paramList() { return paramDocList; }
+
+            int maxNestLevel() { return maxNestLevel; }
+        };
+    }
+
+    private int getMaxNestLevel(final List<ResponseParamDoc> paramDocList) {
+        int level = 1;
+        for (ResponseParamDoc paramDoc : paramDocList) {
+            if (level < paramDoc.nestLevel) {
+                level = paramDoc.nestLevel;
+            }
+        }
+        return level;
+    }
+
+    private void createResponseParameterDocument(final ObjectProperty root,
+                                                 final List<ResponseParamDoc> paramDocList,
+                                                 final int nestLevel) {
+        Map<String, Property> props = root.getProperties();
+        if (props == null) {
+            return;
+        }
+        for (Map.Entry<String, Property> propEntry : props.entrySet()) {
+            String propName = propEntry.getKey();
+            Property prop = propEntry.getValue();
+
+            String type = prop.getType();
+            String format = prop.getFormat();
+            String title = prop.getTitle();
+            String description = prop.getDescription();
+            boolean isRequired = prop.getRequired();
+            ResponseParamDoc paramDoc = new ResponseParamDoc(propName, type, format, title, description, isRequired, nestLevel);
+            paramDocList.add(paramDoc);
+
+            if ("array".equals(type)) {
+                ArrayProperty arrayProp;
+                if (!(prop instanceof  ArrayProperty)) {
+                    continue;
+                }
+                arrayProp = (ArrayProperty) prop;
+                Property itemsProp = arrayProp.getItems();
+                if ("object".equals(itemsProp.getType())) {
+                    createResponseParameterDocument((ObjectProperty) itemsProp, paramDocList, nestLevel + 1);
+                    continue;
+                }
+            } else if ("object".equals(type)) {
+                createResponseParameterDocument((ObjectProperty) prop, paramDocList, nestLevel + 1);
+                continue;
+            }
+        }
+    }
+
+    private Model findDefinition(final Swagger swagger, final String simpleRef) {
+        Map<String, Model> definitions = swagger.getDefinitions();
+        if (definitions == null) {
+            return null;
+        }
+        return definitions.get(simpleRef);
+    }
+
+    private Map<String, Property> getProperties(final Swagger swagger, final ComposedModel parent) {
+        Map<String, Property> result = new HashMap<>();
+        Stack<ComposedModel> stack = new Stack<>();
+        stack.push(parent);
+        do {
+            ComposedModel model = stack.pop();
+            List<Model> children = model.getAllOf();
+            for (Model child : children) {
+                if (child instanceof ModelImpl) {
+                    if (child.getProperties() != null) {
+                        result.putAll(child.getProperties());
+                    }
+                } else if (child instanceof ComposedModel) {
+                    stack.push((ComposedModel) child);
+                } else if (child instanceof RefModel) {
+                    String refName = ((RefModel) child).getSimpleRef();
+                    Model m = findDefinition(swagger, refName);
+                    if (m == null) {
+                        continue;
+                    }
+                    if (m.getProperties() != null) {
+                        result.putAll(m.getProperties());
+                    }
+                }
+            }
+        } while (!stack.empty());
+        return result;
+    }
+
     private interface ProfileDocs {
         String profileName();
+    }
+
+    private class ResponseParamDoc {
+        final String name;
+        final String type;
+        final String format;
+        final String title;
+        final String description;
+        final boolean isRequired;
+        final int nestLevel;
+        int maxNestLevel;
+
+        ResponseParamDoc(final String name,
+                         final String type,
+                         final String format,
+                         final String title,
+                         final String description,
+                         final boolean isRequired,
+                         final int nestLevel) {
+            this.name = name;
+            this.type = type;
+            this.format = format;
+            this.title = title;
+            this.description = description;
+            this.isRequired = isRequired;
+            this.nestLevel = nestLevel;
+        }
+
+        String required() {
+            return this.isRequired ? "Yes" : "No";
+        }
+
+        void setMaxNestLevel(final int max) {
+            this.maxNestLevel = max;
+        }
+
+        int colSpan() {
+            return this.maxNestLevel - this.nestLevel + 1;
+        }
+
+        List<Object> indents() {
+            List<Object> indents = new ArrayList<>();
+            for (int cnt = 0; cnt < this.nestLevel - 1; cnt++) {
+                indents.add("");
+            }
+            return indents;
+        }
     }
 }
