74e178c15bf6c207a45ee59296b7c4a0f3dd2872
Masaru Takano <mtaka@gclue.jp>
2017-06-22 14:43:22 +0900

    Windows用バッチファイルを追加。

---
+++ DeviceConnectCodegen/samples/md-docs.bat
@@ -0,0 +1,16 @@
+:: スケルトンコード種別: APIリファレンス (マークダウン形式)
+set LANG=deviceConnectMarkdownDocs
+
+:: プロファイル定義ファイル
+set SPEC=..\standard-profile-specs
+
+:: スケルトンコード出力先
+set OUTPUT_DIR=.\output\markdown\Device_Connect_RESTful_API_Specification
+
+:: node-gotapiプラグインの表示名
+set DISPLAY_NAME=Device_Connect_RESTful_API_Specification
+
+:: スケルトンコード生成ツールのバイナリ
+set JAR_FILE=..\bin\deviceconnect-codegen.jar
+
+java -Dfile.encoding=UTF-8 -jar %JAR_FILE% --input-spec-dir %SPEC%  --lang %LANG%  --display-name %DISPLAY_NAME%  --output %OUTPUT_DIR%
