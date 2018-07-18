0bead6eb01783602f39615f4746ebe1595e1b8c1
Masaru Takano <mtaka@gclue.jp>
2018-07-18 10:19:45 +0900

    ライセンス表記を追加。

--- DeviceConnectCodegen/modules/deviceconnect-codegen/src/main/java/org/deviceconnect/codegen/DConnectPath.java
+++ DeviceConnectCodegen/modules/deviceconnect-codegen/src/main/java/org/deviceconnect/codegen/DConnectPath.java
@@ -1,3 +1,9 @@
+/*
+ DConnectPath.java
+ Copyright (c) 2018 NTT DOCOMO,INC.
+ Released under the MIT license
+ http://opensource.org/licenses/mit-license.php
+ */
 package org.deviceconnect.codegen;
 
 
--- DeviceConnectCodegen/modules/deviceconnect-codegen/src/main/java/org/deviceconnect/codegen/plugin/IosPluginCodegenConfig.java
+++ DeviceConnectCodegen/modules/deviceconnect-codegen/src/main/java/org/deviceconnect/codegen/plugin/IosPluginCodegenConfig.java
@@ -1,3 +1,9 @@
+/*
+ IosPluginCodegenConfig.java
+ Copyright (c) 2018 NTT DOCOMO,INC.
+ Released under the MIT license
+ http://opensource.org/licenses/mit-license.php
+ */
 package org.deviceconnect.codegen.plugin;
 
 
