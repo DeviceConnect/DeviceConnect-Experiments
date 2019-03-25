6670c418df2c8d2f1b2b15acca8313733fb32caf
Masaru Takano <mtaka@gclue.jp>
2019-03-25 21:35:13 +0900

    バージョン更新スクリプトを修正。
    DeviceConnectCodegen フォルダ直下で tools/version.py を実行できるようにした。

--- DeviceConnectCodegen/tools/version.py
+++ DeviceConnectCodegen/tools/version.py
@@ -1,6 +1,7 @@
 import argparse
 import xml.etree.ElementTree as ET
 import re
+import os
 
 def updatePOM(pomFile, version):
     ET.register_namespace('', 'http://maven.apache.org/POM/4.0.0')
@@ -9,6 +10,7 @@ def updatePOM(pomFile, version):
     versionElement = root.find('./pom:version', {'pom':'http://maven.apache.org/POM/4.0.0'})
     versionElement.text = version
     tree.write(pomFile, encoding="UTF-8")
+    print 'Changed: ' + pomFile
 
 def updateREADME(filename, version):
     with open(filename, 'r') as file:
@@ -18,17 +20,21 @@ def updateREADME(filename, version):
     filedata = re.sub(r'(deviceconnect-codegen-project-).+(-dist\.zip)', repl, filedata)
     filedata = re.sub(r'(https://github.com/.+/DeviceConnect-Experiments/releases/tag/codegen-v).+(\))', repl, filedata)
 
+
     with open(filename, 'w') as file:
         file.write(filedata)
+    print 'Changed: ' + filename
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("version", type=str, help="version name of deviceconnect-codegen")
     args = parser.parse_args()
 
-    updatePOM('../pom.xml', args.version)
-    updatePOM('../modules/deviceconnect-codegen/pom.xml', args.version)
-    updateREADME('../README.md', args.version)
+    baseDir = os.path.dirname(os.path.abspath(__file__))
+    updatePOM(os.path.realpath(baseDir + '/../pom.xml'), args.version)
+    updatePOM(os.path.realpath(baseDir + '/../modules/deviceconnect-codegen/pom.xml'), args.version)
+    updateREADME(os.path.realpath(baseDir + '/../README.md'), args.version)
+    print 'Completed'
 
 if __name__ == "__main__":
     main()
